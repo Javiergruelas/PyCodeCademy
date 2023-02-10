6d08c5c5dd9c25ed800f5bb710e9804e5e49ca74
Javier <javiergruelas@gmail.com>
2023-02-09 19:53:39 -0800

    added another cleaner more readable way to process

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -5,3 +5,9 @@ for grade in grades:
   scaled_grades.append(grade + 10)
 
 print(scaled_grades)
+
+# a cleaner and shorter way to produce the same outcome could be: 
+
+gradesTwo = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_gradesTwo = [grade + 10 for grade in gradesTwo]
+print(scaled_gradesTwo)
