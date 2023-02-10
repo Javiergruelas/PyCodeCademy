e91f615d275221adcc543ec33e8e0e1d7062496e
Javier <javiergruelas@gmail.com>
2023-02-09 20:01:56 -0800

    added can ride coaster section

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -11,3 +11,13 @@ print(scaled_grades)
 gradesTwo = [90, 88, 62, 76, 74, 89, 48, 57]
 scaled_gradesTwo = [grade + 10 for grade in gradesTwo]
 print(scaled_gradesTwo)
+
+
+
+##We have defined a list heights of visitors to a theme park. In order to ride the Topsy Turvy Tumbletron roller coaster, you need to be above 161 centimeters.
+##Using a list comprehension, create a new list called can_ride_coaster that has every element from heights that is greater than 161.
+
+heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
+can_ride_coaster = [height for height in heights if height > 161]
+
+print(can_ride_coaster)
