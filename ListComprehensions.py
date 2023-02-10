31daeb7f289a3238571f5bc5ee1c013ca8036396
Javier <javiergruelas@gmail.com>
2023-02-10 00:53:47 -0800

    added the review or loops section

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -21,3 +21,22 @@ heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 can_ride_coaster = [height for height in heights if height > 161]
 
 print(can_ride_coaster)
+
+#Create a list called single_digits that consists of the numbers 0-9 (inclusive).
+single_digits = list(range(0,10))
+
+#Before the loop, create a list called squares. Assign it to be an empty list to begin with.
+squares = []
+
+#Create the list cubes using a list comprehension on the single_digits list. Each element of cubes should be an element of single_digits taken to the third power.
+cubes = [digit**3 for digit in single_digits]
+
+#Inside the loop that iterates through single_digits, append the squared value of each element of single_digits to the list squares. You can do this before or after printing the element.
+
+for digit in single_digits:
+  print(digit)
+  squares.append(digit**2)
+
+print(squares)
+print(cubes)
+
