3e59d00b0c2ad9f8686b381764c7e65f8d94c3c1
Javier <javiergruelas@gmail.com>
2023-02-09 19:02:29 -0800

    Added the IndexListLoop to the python learning

---
+++ IndexListLoop.py
@@ -0,0 +1,12 @@
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+#get the length of the list
+length = len(python_topics)
+#set index to zero
+index = 0
+
+#loop using index<length
+while index < length:
+  print("I am learning about " + str(python_topics[index]))
+#add one to the index count
+  index += 1
